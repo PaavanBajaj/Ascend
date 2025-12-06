5b09fc034148640eae30c6fc906f88a25de9da3f
Paavan Bajaj <paavanbajaj@yahoo.com>
2025-12-05 21:10:30 -0800

    Add 6:00 PM time slot for Sunday

--- AscendPage/client/src/pages/ScheduleSession.tsx
+++ AscendPage/client/src/pages/ScheduleSession.tsx
@@ -10,7 +10,7 @@ const DAYS = ["Sunday", "Saturday", "Tuesday"] as const;
 type Day = typeof DAYS[number];
 
 const TIME_SLOTS: Record<Day, string[]> = {
-  Sunday: ["10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"],
+  Sunday: ["10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"],
   Saturday: ["10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"],
   Tuesday: ["5:00 PM", "6:00 PM", "7:00 PM"],
 };
